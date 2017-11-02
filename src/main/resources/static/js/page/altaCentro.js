e72706796d6498c4250d6ccb1b178aee3a96be53
Fausto Guillermo <f.seturino@gmail.com>
2017-11-01 19:20:53 -0600

    Desarrollo de pantallas de alta de centro
    Se continua con el desarrollo de las pantallas de consultas.

---
+++ src/main/resources/static/js/page/altaCentro.js
@@ -0,0 +1,45 @@
+//variables de los Bootbox
+var loading;
+var nomPath;
+// ############################
+// init Docunemt ready
+// ############################
+$(document).ready(function () {
+    // ========================
+    // Obtener contexto
+    // ========================
+    nomPath = window.location.pathname;
+    nomPath = nomPath.substring(1, nomPath.length);
+    nomPath = nomPath.split("/", 1);
+    nomPath = nomPath + "/";
+});
+
+//Función para consultar los datos de auditoría.
+function auditoria(){
+	$("#bsfOperadorAuditoria").val(bsfOp);
+	$("#formAuditoria").submit();
+}
+
+//Función para verificar el origen del apunte.
+function origen(){
+	if($("#concepto").val().includes("LIQ")||$("#concepto").val().includes("EMI")){
+		$("#bsfOperadorOrigen").val(bsfOp);
+		$("#formOrigen").submit();
+	}
+	else{
+		msjAlerta("El registro que desea verificar no cuenta con información de ORIGEN.");
+	}
+	
+}
+
+//Funcion para pintar la ventana de mensaje emergente.
+//PARAM text Variable que contiene la cadena que se mostrara en el mensaje.
+function msjAlerta(text) {
+  bootbox.alert({
+  	message : '<p style="overflow: hidden; float: left; margin-left: 5%;" class="">' + '<img style="margin: -220px 0px -240px 0px;" src="./img/messages-g.png" /></p>'
+		+ '<div class="text-center text-alert"><label>¡Atención!</label><br/>' + '<label>' + text + '</label></div>',
+		callback : function() {
+	
+		}
+  });
+}
